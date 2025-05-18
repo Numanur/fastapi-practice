f2a2d13d305edcf9e69a1c318dca55dc5c4dcbc9
Numanur <nomanur016@gmail.com>
2025-05-18 23:46:27 +0600

    query param updated

--- queryparam.py
+++ queryparam.py
@@ -14,3 +14,22 @@ If a function parameter is not part of the path, and
 It has a default value (like skip: int = 0)
 Then FastAPI automatically interprets it as a query parameter.'''
 
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: str | None = None):
+    if q:
+        return {"item_id": item_id, "q": q}
+    return {"item_id": item_id}
+
+'''In this case, item_id is a path parameter, and q is a query parameter. The q parameter is optional and can be None. If q is provided in the URL, it will be included in the response. If not, it will not be included.'''
+
+@app.get("/itemss/{item_id}")
+async def read_item(item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+'''In this case, item_id is a path parameter, q is an optional query parameter, and short is a boolean query parameter. If short is set to True, the response will not include the long description. If it is set to False or not provided, the long description will be included.'''
